aef735e6b11546c5eba7e54b27462b8a96df9c05
angelagar <ggarcia.angelad@gmail.com>
2025-11-06 02:37:52 -0600

    ft: skl timer

--- mrl_sck.py
+++ mrl_sck.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import time as t
 
 df = pd.read_csv("MCDO/breast-cancer.csv")
 y = df["diagnosis"].map({'M': 1, 'B': 0})
@@ -15,14 +16,24 @@ x_train, x_test, y_train, y_test = train_test_split(
     x_scaled, y, test_size=0.3, random_state=42, shuffle=True
 )
 
-model = LogisticRegression(penalty='l2', solver='lbfgs', max_iter=500)
-model.fit(x_train, y_train)
+model = LogisticRegression(penalty='l2', solver='lbfgs', max_iter=50)
 
+# Tiempo de entrenamiento
+timer1 = t.time()
+model.fit(x_train, y_train)
+timer2 = t.time()
+timetraining = timer2 - timer1
+print(f"Tardo en entrenar {timetraining:.5f} segundos")
+
+# Tiempo de predicción
+timer3 = t.time()
 prob_train = model.predict_proba(x_train)[:, 1]
 prob_test = model.predict_proba(x_test)[:, 1]
-
 pred_train = model.predict(x_train)
 pred_test = model.predict(x_test)
+timer4 = t.time()
+timeprediction = timer4 - timer3
+print(f"Tardo en predecir {timeprediction:.5f} segundos")
 
 exact_train = accuracy_score(y_train, pred_train)
 exact_test = accuracy_score(y_test, pred_test)
@@ -35,12 +46,11 @@ ultimas5 = [f"{p:.4f}" for p in prob_test[-5:]]
 print(f"Primeras 5 probabilidades test: {primeras5}")
 print(f"Últimas 5 probabilidades test: {ultimas5}")
 
-# Predicciones de casos específicos del CSV original
-fila1 = x_scaled[0].reshape(1, -1)
-y_ej1 = y.iloc[0]
+fila1 = x_scaled[20].reshape(1, -1)
+y_ej1 = y.iloc[20]
 prob_caso1 = model.predict_proba(fila1)[0, 1]
 pred_caso1 = "Maligno" if prob_caso1 >= 0.5 else "Benigno"
-print(f"Probabilidad de malignidad para la primera fila del CSV: {prob_caso1:.4f}")
+print(f"Probabilidad de malignidad para la fila 21 del CSV: {prob_caso1:.4f}")
 print(f"Predicción: {pred_caso1}")
 print("Real:", "Benigno" if y_ej1 == 0 else "Maligno")
 
